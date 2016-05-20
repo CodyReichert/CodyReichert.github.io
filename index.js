6c5cbbf28ee424a5283f28970b9e81372ffd94e9
Cody Reichert <cody@reichertbrothers.com>
2016-05-20 10:50:37 -0500

    Move components out of index.js to lib/components

--- index.js
+++ index.js
@@ -3,99 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 import { Router, createMemoryHistory } from 'react-router'
-import { Route, IndexRoute } from 'react-router'
-
-const blogContext = require.context('./posts', true, /\.md$/)
-const blogs = blogContext.keys().map(blogContext).sort((a, b) =>
-    // Import blogs and sort them by date (reverse)
-    new Date(b.meta.date) - new Date (a.meta.date)
-)
-
-function getBlogById(postId) {
-    return blogs.find(b => b.id === postId)
-}
-
-
-
-/*
- * Root component - wraps all other
- */
-class Root extends React.Component {
-    static propTypes = {
-        path: React.PropTypes.string,
-        children: React.PropTypes.node,
-    }
-    render() {
-        return (
-            <div id="outlet">
-                {this.props.children}
-            </div>
-        )
-    }
-}
-
-class About extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Hi, thanks for reaching ABOUT ME</p>
-            </div>
-        )
-    }
-}
-
-class Archive extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>All Blogs</h1>
-                <ul>{blogs.map(b => (
-                        <li key={b.id}>
-                            <a href={`/posts/${b.id}/index.html`}>
-                                {b.meta.title}
-                            </a>
-                        </li>
-                    ))}</ul>
-            </div>
-        )
-    }
-}
-
-/*
- * A single blog post page. Gets route param 'id'
- * in it's props, and finds the correct blog to render
- */
-class Post extends React.Component {
-
-    static propTypes = {
-        params: React.PropTypes.object
-    }
-
-    render() {
-        const content = getBlogById(this.props.params.id)
-        return (
-            <div dangerouslySetInnerHTML={{__html:content.content}}></div>
-        )
-    }
-}
-
-/*
- * Routes
- */
-const routes = (
-    <Route component={Root} path='/'>
-        <IndexRoute component={About}/>
-        <Route path="about" component={About} />
-        <Route path="posts" component={Archive} />
-        <Route path="posts/:id" component={Post} />
-    </Route>
-)
-
+import { routes }   from './lib/routes'
 
 /*
  * Client side render
  */
-if (typeof document !== 'undefined') {
+if (typeof window !== 'undefined') {
     const history = createHistory();
     const outlet = document.getElementById('outlet');
     ReactDOM.render(<Router history={history} routes={routes} />, outlet);
